24f8f0d891dcc5f1113c6e3773398c356a7418e4
Maksym Zuliak <agrh.ndcity@gmail.com>
2022-01-27 11:51:55 +0200

    created tables Post and Likes in DataBase, add some test Posts and Likes, display likes on posts in admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from core import models
+# from .models import Likes, Post
 
-# Register your models here.
+# @admin.register(Likes)
+# class LikeAdmin(admin.ModelAdmin):
+#     pass
+
+# @admin.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+class TabularInlineLikes(admin.TabularInline):
+    model=models.Likes
+
+class PostAdmin(admin.ModelAdmin):
+    inlines=[TabularInlineLikes]
+    list_display = ("title", "user")
+
+
+admin.site.register(models.Post, PostAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from config import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False) # blank=False - нельзя оставлять пустым
+    content = models.TextField(null=False, blank=False)
+    image = models.CharField(max_length=255, null=True, blank=True) # blank=True - можно оставлять пустым
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=False, on_delete=models.SET_NULL)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class Likes(models.Model):
+    post = models.ForeignKey("Post", null=False, blank=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, blank=False, on_delete=models.CASCADE)
+    status = models.BooleanField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
